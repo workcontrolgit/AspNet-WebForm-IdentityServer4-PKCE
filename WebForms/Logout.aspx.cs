4b85cfc00e12a4948c14d4241c283957e101e793
Fuji <fuji.nguyen@workcontrol.com>
2023-01-02 11:11:57 -0500

    Add project files.

---
+++ WebForms/Logout.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+
+namespace WebForms
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Context.GetOwinContext().Authentication.SignOut("oidc", "cookies");
+        }
+    }
+}
